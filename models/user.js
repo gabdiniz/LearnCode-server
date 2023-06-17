d50b555d9fd672c77bafbf32cec90af238a8d586
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-06-17 16:24:24 -0300

    Criação do model User

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("../config/database");
+
+const User = connection.define('user', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  birth: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'user',
+    allowNull: false
+  }
+});
+
+module.exports = User;
