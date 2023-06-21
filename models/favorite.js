239bc458bd658548339b54ffbad600866e411410
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-06-21 14:35:24 -0300

    Criação model Favorite

---
+++ models/favorite.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const { connection } = require("../config/database");
+const User = require("./user");
+const Course = require("./course");
+
+const Favorite = connection.define('favorite', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+    primaryKey: true
+  }
+});
+
+User.hasMany(Favorite);
+Favorite.belongsTo(User);
+
+Course.hasMany(Favorite);
+Favorite.belongsTo(Course);
+
+module.exports = Favorite;
